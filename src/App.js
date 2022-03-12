54b91e5edd078b5b78df20a02ffa690bc3790bdc
Chakradhari jamili <Chakku@Chakradharis-MacBook-Pro.local>
2022-03-12 15:31:33 +0530

    Added right Banner

--- src/App.js
+++ src/App.js
@@ -47,18 +47,22 @@ const mockData = [
 function App() {
   const [userVal, setUserVal] = useState('');
   const [list, setList] = useState([]);
+  const [applied, setApplied] = useState('');
 
   const handleClick = () => {
     if (
       mockData.filter((person) => person.jobDescription === userVal).length > 0
     ) {
       setList(mockData.filter((person) => person.jobDescription === userVal));
+      setApplied('jobDescription');
     } else if (
       mockData.filter((person) => person.jobLocation === userVal).length > 0
     ) {
       setList(mockData.filter((person) => person.jobLocation === userVal));
+      setApplied('jobLocation');
     } else {
       setList([]);
+      setApplied('');
     }
   };
 
@@ -66,42 +70,58 @@ function App() {
 
   return (
     <div className="App">
-      <section className="search-section">
-        <input
-          type="text"
-          placeholder="Type here to search"
-          onChange={(e) => setUserVal(e.target.value)}
-          value={userVal}
-        />
-        <button id="searchButton" onClick={handleClick}>
-          Search
-        </button>
-      </section>
-      <section className="display-search-results">
-        {list &&
-          list.map((person, i) => (
-            <div key={person.id} id={`cusGrid${i + 1}`} className="cusGrid">
-              <div>
-                <span>Name: {person.name}</span>
-                <span>Job Descripton: {person.jobDescription}</span>
-              </div>
+      <div>
+        <section className="search-section">
+          <input
+            type="text"
+            placeholder="Type here to search"
+            onChange={(e) => setUserVal(e.target.value)}
+            value={userVal}
+            className="search-input-field"
+            size="50"
+          />
+          <button id="searchButton" onClick={handleClick}>
+            Search
+          </button>
+        </section>
+        <section className="display-search-results">
+          {list &&
+            list.map((person, i) => (
+              <div key={person.id} id={`cusGrid${i + 1}`} className="cusGrid">
+                <div>
+                  <span>Name: {person.name}</span>
+                  <span>Job Descripton: {person.jobDescription}</span>
+                </div>
 
-              <div>
-                <span>From Date: {person.fromDate}</span>
-                <span>To Date: {person.toDate}</span>
-              </div>
+                <div>
+                  <span>From Date: {person.fromDate}</span>
+                  <span>To Date: {person.toDate}</span>
+                </div>
 
-              <div>
-                <span>Wage per day: {person.wagePerDay}</span>
-                <span>Job Location: {person.jobLocation}</span>
-              </div>
+                <div>
+                  <span>Wage per day: {person.wagePerDay}</span>
+                  <span>Job Location: {person.jobLocation}</span>
+                </div>
 
-              <div>
-                <span>Phone Number: {person.phoneNumber}</span>
+                <div>
+                  <span>Phone Number: {person.phoneNumber}</span>
+                </div>
               </div>
-            </div>
-          ))}
-      </section>
+            ))}
+        </section>
+      </div>
+
+      {applied && (
+        <article>
+          <p>Total number of available</p>
+          <span>{userVal}</span>&nbsp;&nbsp;
+          <span>-</span>
+          <span>
+            &nbsp;
+            {mockData.filter((person) => person[applied] === userVal).length}
+          </span>
+        </article>
+      )}
     </div>
   );
 }
